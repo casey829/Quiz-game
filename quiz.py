922a1be3f06abb074536dd2f13199c1e4c74193a
casey829 <caseymwangi31@gmail.com>
2024-10-17 13:17:24 +0300

    adding questions

--- quiz.py
+++ quiz.py
@@ -1,1 +1,121 @@
-print ("");
+print ("Welcome to my computer Quiz!")
+
+playing = input("Do you want to play? ")
+
+
+if playing != "yes":
+    quit()
+    
+print("Alright, let the fun begin!!:)")
+
+answer = input("What is the capital of France? ")
+
+if answer == "Paris":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("What is the capital of France? ")
+
+if answer == "Paris":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("Who wrote the play Romeo and Juliet? ")
+
+if answer == "William Shakespeare":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("What is the chemical symbol for water? ")
+
+if answer == "H2O":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("What planet is known as the Red Planet? ")
+
+if answer == "Mars":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("What is the largest mammal in the world? ")
+
+if answer == "Blue whale":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("How many continents are there on Earth?")
+
+if answer == "7":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+answer = input("What is the square root of 64? ")
+
+if answer == "8":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+    
+answer = input("Who painted the Mona Lisa?")
+
+if answer == "Leonardo da Vinci":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+    
+answer = input("What is the fastest land animal? ")
+
+if answer == "Cheetah":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+
+answer = input("In which year did the Titanic sink? ")
+
+if answer == "1912":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+
+answer = input("What is the currency of Japan? ")
+
+if answer == "Yen":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+    
+answer = input("Who was the first person to walk on the moon? ")
+
+if answer == "Neil Armstrong":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+
+
+answer = input("What is the main ingredient in guacamole? ")
+
+if answer == "Avocado":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
+    
+
+answer = input("What is the most spoken language in the world? ")
+
+if answer == "Mandarin Chinese":
+    print("Correct!!")
+else:
+    print("Incorrect!:(")
